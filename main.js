46e3349aee4e5162f3592ae1e8bacb8fc1c38f12
bee106 <davidocano52@gmail.com>
2024-01-08 08:28:31 -0500

    Product detail style

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const mobileMenu = document.querySelector('.mobile-menu')
 const iconMenu = document.querySelector('.menu')
 
 const carIcon = document.querySelector('.navbar-shopping-cart')
-const productDetail = document.querySelector('.product-detail')
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer')
 
  const containerCard= document.querySelector('.cards-container');
 
@@ -14,24 +14,26 @@ navbarEmail.addEventListener('click', toggleDesktopMenu)
 
 
 iconMenu.addEventListener('click', toggleMenuHam)
+
+
 carIcon.addEventListener('click', toggleCar)
 
 
 function toggleDesktopMenu() {
-  const isProductDetailClose = productDetail.classList.contains('inactive_menu')
+  const isProductDetailClose = shoppingCartContainer.classList.contains('inactive_menu')
   if (!isProductDetailClose) {
-    productDetail.classList.add('inactive_menu')
+    shoppingCartContainer.classList.add('inactive_menu')
   }
   desktopMenu.classList.toggle('inactive_menu')
 }
 function toggleMenuHam() {
 
-  const isProductDetailClose = productDetail.classList.contains('inactive_menu')
+  const isProductDetailClose = shoppingCartContainer.classList.contains('inactive_menu')
   if (!isProductDetailClose) {
-    productDetail.classList.add('inactive_menu')
+    shoppingCartContainer.classList.add('inactive_menu')
   }
   mobileMenu.classList.toggle('inactive_menu')
-
+  
 }
 function toggleCar() {
 
@@ -40,7 +42,7 @@ function toggleCar() {
   if (!isMobileMenuClose) {
     mobileMenu.classList.add('inactive_menu')
   }
-  productDetail.classList.toggle('inactive_menu')
+  shoppingCartContainer.classList.toggle('inactive_menu')
 
 
 
@@ -49,84 +51,6 @@ function toggleCar() {
 
 // new productos
 
-
-
-{/* <div class="product-card">
-<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
-<div class="product-info">
-  <div>
-    <p>$120,00</p>
-    <p>Bike</p>
-  </div>
-  <figure>
-    <img src="./icons/bt_add_to_cart.svg" alt="">
-  </figure>
-</div>
-</div> */}
-// const productsList =[
-
-// ]
-// productsList.push({
-//     name :'bike',
-//     price : '120.000',
-//     img: 'https:images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-// })
-
-// productsList.push({
-//   name :'Guantes',
-//   price :'12.000',
-//   img: 'https:images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-// })
-
-// productsList.push({
-//   name :'Casco',
-//   price : '20.000',
-//   img: 'https:images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-// })
-
-// for( product of productsList){
-
-//   // div container
-//    const productCar = document.createElement('div')
-//   productCar.classList.add('product-card')
-
-// // img product
-
-// const imgSection = document.createElement('img')
-// imgSection.setAttribute('src', product.img)
-
-
-// // product info
-// const productInfo = document.createElement('div')
-//   productCar.classList.add('product-info')
-
-//   // div vacio
-//   const productInfoDiv = document.createElement('div')
-
-//   const parrafo1 = document.createElement('p')
-//   parrafo1.innerHTML = '$' + product.price 
-//   const parrafo2 = document.createElement('p')
-//   parrafo2.innerHTML = product.name
-
-
-//  productInfoDiv.appendChild(parrafo1)
-//  productInfoDiv.appendChild(parrafo2)
-
-
-
-//   const productFigure = document.createElement('figure')
-//   const imgCar = document.createElement('img')
-//   imgCar.setAttribute('src', './icons/bt_add_to_cart.svg')
-
-//   productFigure.appendChild(imgCar)
-//  productCar.appendChild(imgSection)
-//   productInfo.appendChild(productInfoDiv)
-//   productInfo.appendChild(productFigure)
- 
-//   productCar.appendChild(productInfo)
-
-// containerCard.appendChild(productCar)
-// }
 const productList = [];
 productList.push({
   name: 'Bike', price: 120, image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
